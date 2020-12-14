22d3b9c723315854da1c3d24b3fc4805b91d871f
Kyle ROBINSON <w021256j@student.staffs.ac.uk>
2020-12-14 17:06:20 +0000

    Assets added to the solution.

--- PaintApp/Program.cs
+++ PaintApp/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace PaintApp
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
             Client client = new Client();
             if ( client.Connect( "127.0.0.1", 4444 ) == true )
--- Server/Program.cs
+++ Server/Program.cs
@@ -2,7 +2,7 @@
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
             Server server = new Server( "127.0.0.1", 4444 );
             server.Start();
